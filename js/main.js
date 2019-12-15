3e094e00fac66909745c8467a3619427e8c3067f
amsmite001 <smite.am@outlook.com>
2019-12-14 19:27:35 -0700

    reenter web2

--- js/main.js
+++ js/main.js
@@ -15,3 +15,26 @@ $(document).ready(function() {
     });
     
   });
+  
+(function($) {
+    $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 56
+  });
+
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  navbarCollapse();
+  $(window).scroll(navbarCollapse);
+
+})(jQuery);
+
